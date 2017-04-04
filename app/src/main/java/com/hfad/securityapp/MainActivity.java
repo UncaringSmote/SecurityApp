16167642be7b4b3c0379b98e00223b84e3066698
D <pnu008@aol.com>
2017-04-04 19:46:05 -0400

    added message receiving

--- app/src/main/java/com/hfad/securityapp/MainActivity.java
+++ app/src/main/java/com/hfad/securityapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.hfad.securityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
     }
 }
